f4fde3b685d1413205724e978b99f52b738ccab5
arah <p.arah@alustudent.com>
2021-04-07 02:30:27 +0200

    add custome message

--- src/App.js
+++ src/App.js
@@ -19,19 +19,9 @@ function App() {
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+             Coming Soon! You can reach out via email: community@aluopensource.com
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
           </VStack>
         </Grid>
       </Box>
